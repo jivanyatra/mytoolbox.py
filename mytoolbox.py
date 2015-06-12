9f82427c09f294b64fe3f3fee3b4049265dd54c1
Yatri <jivanyatra@gmail.com>
2015-06-12 20:43:01 +0000

    added more documentation for read_words()

--- mytoolbox.py
+++ mytoolbox.py
@@ -6,7 +6,11 @@ functions, paradigms, etc. in this module for use later in projects.
 def read_words(file_name, delimiter=''):
     '''A generator that parses a file and yields words.
     Call this function with a filename to be opened as read-only,
-    and specify an optional delimiter for splitting lines into words.'''
+    and specify an optional delimiter for splitting lines into words.
+    
+    Can be handed to collections.Counter() to count word frequency.
+    Can be used in a genexp in any() to return words belonging to a subset.
+    '''
     with open(file_name, 'r') as f:
         for line in f:
             for word in line.split(delimiter):
