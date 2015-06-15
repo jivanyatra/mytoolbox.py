5425699d7e9f9e6bdc6fe70c5e097768bc8a8cf7
Yatri <jivanyatra@gmail.com>
2015-06-15 22:36:07 +0000

    added fibonacci iterator (maybe switch to generator later?)

--- mytoolbox.py
+++ mytoolbox.py
@@ -16,3 +16,20 @@ def read_words(file_name, delimiter=''):
             for word in line.split(delimiter):
                 yield word
 
+class Fib(max):
+    '''Fibonacci sequence iterator, goes until (but not including) max'''
+
+    def __init__(self, max):
+        self.max = max
+
+    def __iter__(self):
+        self.a = 0
+        self.b = 1
+        return self
+
+    def next(self):
+        fib = self.a
+        if fib > self.max:
+            raise StopIteration
+        self.a, self.b = self.b, self.a + self.b
+        return fib
